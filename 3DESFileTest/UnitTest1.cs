9a4983e41e60e9512d93a7c3024580e555d932fb
harisdean <harisdean@me.com>
2020-03-21 16:35:31 +0800

    final
    the changes

--- 3DESFileTest/UnitTest1.cs
+++ 3DESFileTest/UnitTest1.cs
@@ -1,15 +1,15 @@
-using System;
-using Xunit;
-using _3DESFile;
+//using System;
+//using Xunit;
+//using _3DESFile;
 
-namespace _3DESFileTest
-{
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
-            //Program.
-        }
-    }
-}
+//namespace _3DESFileTest
+//{
+//    public class UnitTest1
+//    {
+//        [Fact]
+//        public void Test1()
+//        {
+//            //Program.
+//        }
+//    }
+//}
